27bce21e27d88ff4fc34f4e5bc581e5bd33d9ff6
Benjamim Sonntag <benjamimsonntag@gmail.com>
2019-04-06 17:37:00 +0100

    v0.1.0

--- dist/index.js
+++ dist/index.js
@@ -1,40 +1,55 @@
-'use strict';
+"use strict";
 
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
+exports["default"] = void 0;
 
-var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+var _react = require("react");
 
-var _react = require('react');
+var _propTypes = _interopRequireDefault(require("prop-types"));
 
-var _propTypes = require('prop-types');
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
-var _propTypes2 = _interopRequireDefault(_propTypes);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
 
-function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
 
-var DeviceMotion = function (_Component) {
+function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
+
+function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+
+function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
+
+function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
+var DeviceMotion =
+/*#__PURE__*/
+function (_Component) {
   _inherits(DeviceMotion, _Component);
 
   function DeviceMotion() {
-    var _ref;
+    var _getPrototypeOf2;
 
-    var _temp, _this, _ret;
+    var _this;
 
     _classCallCheck(this, DeviceMotion);
 
-    for (var _len = arguments.length, args = Array(_len), _key = 0; _key < _len; _key++) {
+    for (var _len = arguments.length, args = new Array(_len), _key = 0; _key < _len; _key++) {
       args[_key] = arguments[_key];
     }
 
-    return _ret = (_temp = (_this = _possibleConstructorReturn(this, (_ref = DeviceMotion.__proto__ || Object.getPrototypeOf(DeviceMotion)).call.apply(_ref, [this].concat(args))), _this), _this.state = {
+    _this = _possibleConstructorReturn(this, (_getPrototypeOf2 = _getPrototypeOf(DeviceMotion)).call.apply(_getPrototypeOf2, [this].concat(args)));
+
+    _defineProperty(_assertThisInitialized(_this), "state", {
       acceleration: {
         x: 0,
         y: 0,
@@ -51,29 +66,37 @@ var DeviceMotion = function (_Component) {
         beta: 0,
         gamma: 0
       }
-    }, _this.handleDeviceMotion = function (event) {
+    });
+
+    _defineProperty(_assertThisInitialized(_this), "handleDeviceMotion", function (event) {
       var acceleration = event.acceleration,
           accelerationIncludingGravity = event.accelerationIncludingGravity,
           interval = event.interval,
           rotationRate = event.rotationRate;
 
+      _this.setState({
+        acceleration: acceleration,
+        accelerationIncludingGravity: accelerationIncludingGravity,
+        interval: interval,
+        rotationRate: rotationRate
+      });
+    });
 
-      _this.setState({ acceleration: acceleration, accelerationIncludingGravity: accelerationIncludingGravity, interval: interval, rotationRate: rotationRate });
-    }, _temp), _possibleConstructorReturn(_this, _ret);
+    return _this;
   }
 
   _createClass(DeviceMotion, [{
-    key: 'componentDidMount',
+    key: "componentDidMount",
     value: function componentDidMount() {
       window.addEventListener('devicemotion', this.handleDeviceMotion, true);
     }
   }, {
-    key: 'componentWillUnmount',
+    key: "componentWillUnmount",
     value: function componentWillUnmount() {
       window.removeEventListener('devicemotion', this.handleDeviceMotion, true);
     }
   }, {
-    key: 'render',
+    key: "render",
     value: function render() {
       return this.props.children(this.state);
     }
@@ -82,12 +105,14 @@ var DeviceMotion = function (_Component) {
   return DeviceMotion;
 }(_react.Component);
 
-DeviceMotion.propTypes = {
-  children: _propTypes2.default.func
-};
-DeviceMotion.defaultProps = {
+exports["default"] = DeviceMotion;
+
+_defineProperty(DeviceMotion, "propTypes", {
+  children: _propTypes["default"].func
+});
+
+_defineProperty(DeviceMotion, "defaultProps", {
   children: function children() {
     return null;
   }
-};
-exports.default = DeviceMotion;
+});
